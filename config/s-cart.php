61765ec7c53fc084c34d71fff2cce0a3181683fe
Trường Phạm Văn <84899576+truongkun@users.noreply.github.com>
2024-03-15 21:51:16 +0700

    15/03/2024

---
+++ config/s-cart.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'version'     => '6.9',
+    'sub-version' => '6.9.3',
+    'type'        => 'basic',
+    'homepage'    => 'https://s-cart.org',
+    'title'       => 'Free Open Source eCommerce for Business',
+];
